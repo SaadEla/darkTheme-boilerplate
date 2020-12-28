010390aca7c0434d3447bc2efba5fd1cc4d0e3bb
s.elaouani <s.elaouani@piman-group.fr>
2020-12-28 21:02:48 +0100

    WIP darkthem setup

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,22 @@
+import { React } from 'react';
+import './header.styles.scss'
+
+const Header = (props) => {
+    const {toggleDarkTheme, changeTheme } = props;
+    return (
+        <div className="header">
+            <nav className="navbar">
+                <div className="navbar-brand">
+                    Dark Theme Toogler
+                </div>
+                <div className="themeSwitch">
+                    <button type="button" className="btn btn-dark" onClick={()=>changeTheme()}>
+                        {toggleDarkTheme ? "Light" : "Dark"}
+                    </button>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Header;
